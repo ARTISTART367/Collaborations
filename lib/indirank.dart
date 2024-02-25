3bb5cf003c294f583a93fcc579469deea2cce8fb
ARTISTART367 <143731557+ARTISTART367@users.noreply.github.com>
2024-02-25 16:05:17 +0530

    List

--- lib/indirank.dart
+++ lib/indirank.dart
@@ -1,34 +1,62 @@
 import 'package:flutter/material.dart';
 
 class IndiRank extends StatelessWidget {
-  const IndiRank({super.key});
+  const IndiRank({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var arrNames = ['Rohan', 'Raj', 'Jay'];
     return Scaffold(
       appBar: AppBar(
         title: const Text('Rankings'),
         backgroundColor: Colors.amber,
+        centerTitle: true,
       ),
       body: SizedBox(
         width: 1000,
-        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          OutlinedButton(
-            child: const Text('Individual Rankigs'),
-            onPressed: () {
-              Navigator.pushNamed(
-                  context,
-                  MaterialPageRoute(builder: (context) => const IndiRank())
-                      as String);
-            },
-          ),
-          OutlinedButton(
-            child: const Text('Locality Ranking'),
-            onPressed: () {
-              Navigator.pushNamed(context, 'localirank');
-            },
-          ),
-        ]),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                OutlinedButton(
+                  child: const Text('Individual Rankings'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const IndiRank()),
+                    );
+                  },
+                ),
+                OutlinedButton(
+                  child: const Text('Locality Ranking'),
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'localirank');
+                  },
+                ),
+              ],
+            ),
+            Expanded(
+              child: ListView.separated(
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Text('$index'),
+                    title: Text(arrNames[index]),
+                    subtitle: Text('Number'),
+                    trailing: Icon(Icons.numbers),
+                  );
+                },
+                itemCount: arrNames.length,
+                separatorBuilder: (context, index) {
+                  return Divider(
+                    height: 10, // Reduced height for the divider
+                    thickness: 1,
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
