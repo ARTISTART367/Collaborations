aa92dd6abe788a701a8535355345ce792d8f1e07
ARTISTART367 <143731557+ARTISTART367@users.noreply.github.com>
2024-02-25 19:13:24 +0530

    more icons

--- lib/appdrawer.dart
+++ lib/appdrawer.dart
@@ -5,6 +5,7 @@ class AppDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // int _selected = 0;
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
@@ -18,8 +19,30 @@ class AppDrawer extends StatelessWidget {
             decoration: BoxDecoration(color: Color.fromARGB(255, 17, 53, 1)),
           ),
           ListTile(
-            leading: Icon(Icons.file_upload),
-          )
+            // selected: _selected == 0,
+            leading: Icon(Icons.science),
+            title: Text('DiscoverySphere'),
+          ),
+          ListTile(
+            leading: Icon(Icons.money_off_rounded),
+            title: Text('TradeVista'),
+          ),
+          ListTile(
+            leading: Icon(Icons.architecture),
+            title: Text('Creativium'),
+          ),
+          ListTile(
+            leading: Icon(Icons.science),
+            title: Text('Students'),
+          ),
+          ListTile(
+            leading: Icon(Icons.nature),
+            title: Text('NGOs'),
+          ),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('Logout'),
+          ),
         ],
       ),
     );
